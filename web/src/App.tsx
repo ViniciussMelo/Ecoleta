379feaa3da4804190f5c057d71839106a8aa3edc
Vinicius Spada Melo <vinicius.smelo54@gmail.com>
2020-06-04 13:30:01 -0300

    Add: Class 03, routes and pages: Home, CreatePoint

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 
-import Home from "./pages/Home";
+import Routes from "./routes";
 
 function App() {
   return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
+      <Routes />
   );
 }
 
